c69c9771ab3d65013236c6b3abc9743578e64d14
kuro-kano <66070091@kmitl.ac.th>
2025-02-12 00:34:01 +0700

    fixed: easy to read index.js and make login.html more clean

--- Week-9/index.js
+++ Week-9/index.js
@@ -18,35 +18,29 @@ app.set('view engine', 'ejs');
 
 // routing
 
-app.get('/register', function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/register.html"));
-});
+/**** lab9/1 ****/
+app.get('/create', function (req, res) {
+    let sql = `CREATE TABLE users ( \
+    username VARCHAR(20) NOT NULL PRIMARY KEY, \
+    password VARCHAR(20), \
+    email VARCHAR(20), \
+    firstname VARCHAR(20), \
+    lastname VARCHAR(20), \
+    age INT(3), \
+    address VARCHAR(50), \
+    phone VARCHAR(15) \
+    );`
 
-app.get('/login', function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/login.html"));
-})
-
-app.get('/process_get', function (req, res) {
-    let formdata = {
-        username: req.query.username,
-        password: req.query.password,
-        email: req.query.email,
-        firstname: req.query.firstname,
-        lastname: req.query.lastname,
-        age: req.query.age,
-        address: req.query.address,
-        phone: req.query.phone,
-    };
-    console.log(formdata);  
-
-    let sql = `INSERT INTO users VALUES ('${formdata.username}', '${formdata.password}', '${formdata.email}', '${formdata.firstname}','${formdata.lastname}', ${formdata.age}, '${formdata.address}', '${formdata.phone}');`;
-
-    console.log(sql);
     conn.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("a record inserted");
-    });
-});
+        console.log("Created successfully.");
+    })
+})
+
+/***** lab9/2 *****/
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/login.html"));
+})
 
 app.get('/process_check', function (req, res) {
 
@@ -65,15 +59,17 @@ app.get('/process_check', function (req, res) {
             // console.log(`saved_passwd: ${result[0].password}, input_passwd: ${logindata.password}`);
             return res.json({ status: "not found", message: "Username or Email not found." });
         } else if (logindata.password != result[0].password) {
-            return res.json({ status: "wrong passwd", message: "Wrong Password, Please try again."})
-        }
-        // console.log(result[0].password);
-        else if (logindata.password == result[0].password) {
-            return res.json({ status: "success", message: `Login Successfully!!`});
+            return res.json({ status: "wrong passwd", message: "Wrong Password, Please try again." })
+        } else if (logindata.password == result[0].password) {
+            return res.json({ status: "success", message: `Login Successfully!!` });
+        } else {
+            return res.json({ status: "error", message: "An error occurred, please try again."});
         }
     });
 })
 
+
+/**** tutorial ****/
 app.get('/show', function (req, res) {
 
     // ให้แสดงข้อมูล จาก table instructor และ teaches
@@ -85,13 +81,15 @@ app.get('/show', function (req, res) {
         if (err) throw err;
         res.render('show', { data: result })
         res.end()
-    }) 
+    })
 
 })
 
+/**** lab9/3 ****/
 app.get('/songs', function (req, res) {
 
-    const sql = 'select s.song_name, a.artist_name as artist, s.song_release_date, s.song_type from songs s join artists a on s.artist = a.artist_id;';
+    const sql = 'select s.song_name, a.artist_name as artist, s.song_release_date, s.song_type \
+    from songs s join artists a on s.artist = a.artist_id;';
     conn.query(sql, function (err, result, fields) {
         if (err) throw err;
         res.render('songs', { data: result })
@@ -100,6 +98,32 @@ app.get('/songs', function (req, res) {
 
 })
 
+/*** ทำเล่น ***/
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/register.html"));
+});
+
+app.get('/process_get', function (req, res) {
+    let formdata = {
+        username: req.query.username,
+        password: req.query.password,
+        email: req.query.email,
+        firstname: req.query.firstname,
+        lastname: req.query.lastname,
+        age: req.query.age,
+        address: req.query.address,
+        phone: req.query.phone,
+    };
+    console.log(formdata);
+
+    let sql = `INSERT INTO users VALUES ('${formdata.username}', '${formdata.password}', '${formdata.email}', '${formdata.firstname}','${formdata.lastname}', ${formdata.age}, '${formdata.address}', '${formdata.phone}');`;
+
+    console.log(sql);
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("a record inserted");
+    });
+});
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
