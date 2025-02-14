23557d847a27c1fcdb8d986557c46a80c8d25beb
kuro-kano <66070091@kmitl.ac.th>
2025-02-14 12:48:46 +0700

    add: form.ejs | add: delete rows from employees

--- Week-10/index.js
+++ Week-10/index.js
@@ -27,9 +27,47 @@ app.get('/show', function (req, res) {
         if (err)
             console.log(err.message);
         console.log(rows);
-        res.render('showdata', { data : rows });
+        res.render('showdata', { data: rows });
     });
 });
+
+app.get('/delete', function (req, res) {
+    // delete a row based on id
+    let sql = `delete from employees where EmployeeId = ${req.query.id}`;
+    console.log(sql);
+
+    db.run(sql, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Row(s) deleted.`);
+    });
+})
+
+app.get('/form', function (req, res) {
+    res.render('form');
+})
+
+app.get('/formget', function (req, res) {
+    let formdata = {
+        id: req.query.id,
+        fname: req.query.fname,
+        lname: req.query.lname,
+        title: req.query.title,
+        phone: req.query.phone,
+        email: req.query.email
+    };
+    console.log(formdata);  
+    //
+    let sql = `INSERT INTO employees values ('${formdata.id}', '${formdata.lname}', '${formdata.fname}', '${formdata.title}', '${formdata.phone}', '${formdata.email}')`;
+    console.log(sql);
+    db.run(sql, (err) => {
+        if (err) {
+            return console.error('Error inserting data:', err.message);
+        }
+        console.log('Data inserted successful');
+    });
+})
 
 app.listen(port, () => {
     console.log("Server started.");
