21b1d586ed9aa7e3e81ca25413bbd05437f8b108
kuro-kano <66070091@kmitl.ac.th>
2025-02-22 18:27:14 +0700

    modified: index.js and debug manop:8000 (result: web kak)

---
+++ Week-11/1. Flag/index.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    const endpoint = "http://10.0.15.21:8000/countries";
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(countries => {
+        console.log(countries);
+        // res.render(asd)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Starting node.js at port ${port}`);
+})
