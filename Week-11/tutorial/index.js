9797dc6f631bb9c64924338ae1e9b668c2aa8251
kuro-kano <66070091@kmitl.ac.th>
2025-02-23 00:09:41 +0700

    I just a little bit changed

--- Week-11/tutorial/index.js
+++ Week-11/tutorial/index.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const path = require("path");
-const port = 3000;
+const port = 5000;
 const sqlite3 = require('sqlite3').verbose();
 
 // Creating the Express server
