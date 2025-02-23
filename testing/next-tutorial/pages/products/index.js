1206b6c8caf9a39a8277d4f3cd74b8f3104d6b4f
kuro-kano <66070091@kmitl.ac.th>
2025-02-24 00:13:23 +0700

    add: fetch API

--- testing/next-tutorial/pages/products/index.js
+++ testing/next-tutorial/pages/products/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head"
+// https://dummyjson.com/products?limit=12
 
-export default function Index(){
+export async function getStaticProps(){
+    const res = await fetch("https://dummyjson.com/products?limit=12");
+    const data = await res.json();
+    console.log(data);
+
+    return {
+        props: { products: data.products }
+    };
+}
+
+export default function Index({products}){
     return (
         <>
             <Head>
