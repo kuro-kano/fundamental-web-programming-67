f81be147eea880c2adf410b695627fe9b00decae
kuro-kano <66070091@kmitl.ac.th>
2025-02-23 23:26:45 +0700

    add: global style

--- testing/next-tutorial/components/Navbar.js
+++ testing/next-tutorial/components/Navbar.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
-        <div> GearGod </div>
-        <Link href='/'>Home </Link>
-        <Link href="/about">About </Link>
-        <Link href="/products">Product </Link>
+      <div className="logo">
+        <Link href="/">
+            <Image src="/favicon.ico" width={50} height={50} alt="logo" />
+        </Link>
+      </div>
+      <Link href="/">Home </Link>
+      <Link href="/about">About </Link>
+      <Link href="/products">Product </Link>
     </nav>
   );
 }
