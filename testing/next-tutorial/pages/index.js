a204bac326c59b154e1f57de3e10a6561dae49e0
kuro-kano <66070091@kmitl.ac.th>
2025-02-23 23:58:06 +0700

    add: Next Head

--- testing/next-tutorial/pages/index.js
+++ testing/next-tutorial/pages/index.js
@@ -1,10 +1,15 @@
 import styles from "@/styles/Home.module.css"
 import Image from "next/image"
 import Link from "next/link"
+import Head from "next/head"
 
 export default function Home(){
     return (
         <>
+            <Head>
+                <title>Home | GearGod</title>
+            </Head>
+
             <div className={styles.container}>
                 <h1 className={styles.title}>Home page</h1>
                 <Image src="globe.svg" width={50} height={50} alt="picture" />
--- testing/next-tutorial/pages/products/index.js
+++ testing/next-tutorial/pages/products/index.js
@@ -1,6 +1,12 @@
+import Head from "next/head"
+
 export default function Index(){
     return (
         <>
+            <Head>
+                <title>Product | GearGod</title>
+            </Head>
+
             <h1>Product</h1>
         </>
     )
