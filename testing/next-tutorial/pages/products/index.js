3434c9a16e2ff4e9a4cf6b12c29417a1288559fe
kuro-kano <66070091@kmitl.ac.th>
2025-02-24 00:49:35 +0700

    add: used data and picture from API

--- testing/next-tutorial/pages/products/index.js
+++ testing/next-tutorial/pages/products/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
-// https://dummyjson.com/products?limit=12
+import Image from "next/image";
+import styles from "@/styles/Product.module.css"
 
 export async function getStaticProps(){
     const res = await fetch("https://dummyjson.com/products?limit=12");
@@ -18,7 +19,14 @@ export default function Index({products}){
                 <title>Product | GearGod</title>
             </Head>
 
-            <h1>Product</h1>
+            <div className={styles.container}>
+                {products.map(item => (
+                    <div key={item.id}>
+                        <h2 className={styles.title}>{item.title}</h2>
+                    <Image className={styles.image} src={item.thumbnail} width={300} height={300} alt="image" />
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
