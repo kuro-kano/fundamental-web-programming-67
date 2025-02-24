bc8e61b009faed54cc24ace6136a8d5d7a23b698
kuro-kano <66070091@kmitl.ac.th>
2025-02-24 12:52:36 +0700

    add: Flag directory

--- Week-11/1. Flag/index.js
+++ Week-11/1. Flag/index.js
@@ -6,18 +6,20 @@ const port = 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('styles'));
+
 app.set('view engine', 'ejs');
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     const endpoint = "http://10.0.15.21:8000/countries";
     fetch(endpoint)
     .then(response => response.json())
-    .then(countries => {
-        console.log(countries);
-        // res.render(asd)
+    .then(ctry => {
+        // * console.log(ctry);
+        res.render("countries", { data: ctry });
     })
     .catch(error => {
         console.log(error);
